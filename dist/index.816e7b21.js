695b933f024156d7045c3df28e5497a470546b57
MoisesCorea <mcoreav@uoc.edu>
2023-05-01 09:59:28 -0600

    Añadiendo Cambios

---
+++ dist/index.816e7b21.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const button = document.getElementById("buttonPortada");
+    button.addEventListener("click", ()=>{
+        window.location.href = "sitios.html";
+    });
+});
+
+//# sourceMappingURL=index.816e7b21.js.map
