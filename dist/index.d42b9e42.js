87122fccfc548da240ce54f6fc97b992896aaa6a
MoisesCorea <mcoreav@uoc.edu>
2023-05-01 01:52:35 -0600

    Añediendo proyecto a GitHub

---
+++ dist/index.d42b9e42.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){document.getElementById("buttonPortada").addEventListener("click",(function(){window.location.href="sitios.html"}))}));
+//# sourceMappingURL=index.d42b9e42.js.map
