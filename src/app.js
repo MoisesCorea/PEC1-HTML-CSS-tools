702ec4ea4ed38146d285e7e48fcc6fbdb5e6fb9a
MoisesCorea <mcoreav@uoc.edu>
2023-05-01 13:21:27 -0600

    Añdiendo ultimos cambios

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('buttonPortada');
+  const button = document.getElementById('buttonPortada')
   button.addEventListener('click', () => {
-    window.location.href = 'sitios.html';
-  });
-});
+    window.location.href = 'sitios.html'
+  })
+})
